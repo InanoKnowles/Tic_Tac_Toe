ccf11dddd67035a04cf0cf69b19202f254d78ff1
inanok <missinanoknowles@gmail.com>
2022-11-24 11:37:28 +1000

    All files separated, draw condition has a bug

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 console.log("test link to html")
-//----------------------------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------------------------------
 var allBoxes =  document.querySelectorAll('.box')
     console.log(allBoxes[0])
 // [div.box, div.box, div.box, div.box, div.box, div.box, div.box, div.box, div.box]
@@ -16,15 +16,50 @@ allBoxes[i].addEventListener('click', function (event) {
     // ! Issue with both X and O on click only one is accepted and that is O
     // 7. We need to be able to tell which click is X and which click is O - How can we do that? My thoughts:
     // - create 2 variables and assign each variable with an X or an O then we could create a rule that if X clicks then its O's turn to click (if)
-    
-    console.log(click)
-    if (click % 2 === 0){
+
+    // if (filled) {
+    //     no touchies
+    // } 
+    // console.log(allBoxes[i].textContent)
+    // console.log(click)
+    if (click % 2 === 0 && allBoxes[i].textContent === " ") {
         allBoxes[i].textContent="O"
         click++
-    } else {
+    } else if (allBoxes[i].textContent === " ") {
         allBoxes[i].textContent="X"
         click++
     }
+
+    // Step 1 -  what coding structure? if statement
+    // Step 2 -  what condition - all the divs called box need to have a value
+    // Step 3 do what - if all filled declare this round a draw
+
+    // console.log(allBoxes[0].textContent)
+    // console.log(allBoxes[1].textContent)
+    // console.log(allBoxes[2].textContent)
+    // console.log(allBoxes[3].textContent)
+    // console.log(allBoxes[4].textContent)
+    // console.log(allBoxes[5].textContent)
+    // console.log(allBoxes[6].textContent)
+    // console.log(allBoxes[7].textContent)
+    // console.log(allBoxes[8].textContent)
+
+    if (allBoxes[0].textContent !== " "
+     && allBoxes[1].textContent !== " "
+     && allBoxes[2].textContent !== " "
+     && allBoxes[3].textContent !== " "
+     && allBoxes[4].textContent !== " "
+     && allBoxes[5].textContent !== " "
+     && allBoxes[6].textContent !== " "
+     && allBoxes[7].textContent !== " "
+     && allBoxes[8].textContent !== " "){
+        alert("This round is a DRAW - Good Game")
+    }
+    // if (document.querySelectorAll('.box').textContent !== " ") {
+     
+    //     console.log("This round was a draw - Good Game!")
+    // }
+    console.log(allBoxes[0].textContent)
     // We used modulus 2 because that will give us remainder 0 for every even number and if its an odd number it gives remainder 1
     // There are 8 winning conditions:
     if (document.querySelector('#box1').textContent !== " " && document.querySelector('#box1').textContent === document.querySelector('#box2').textContent && document.querySelector('#box3').textContent === document.querySelector('#box1').textContent) {
